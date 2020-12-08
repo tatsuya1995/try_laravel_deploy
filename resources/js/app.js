3dc922f25fa6e47eb6be750af63522bd9d03ad89
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-08 11:17:14 +0900

    pusher機能追加　テスト

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,24 @@
 require('./bootstrap');
 
-$(docummnt).ready(function(){
-    $.ajaxSetup({
-        headers: {
-            "X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")
-        }
-    });
-    $("#submit").click(function(){
-        const url = "/pusher/create";
-        $.ajax({
-            url: url,
-            data: {
-                comment: $("#comment").val(),
-            },
-            method: "POST"
-        });
-        return false;
-    });
-    window.Echo.channel("chat")
-        .listen("Pusher",e =>{
-        $("#board").append("<li>" + e.chat.comment + "</li>");
-    });
-});
+// $(docummnt).ready(function(){
+//     $.ajaxSetup({
+//         headers: {
+//             "X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")
+//         }
+//     });
+//     $("#submit").click(function(){
+//         const url = "/pusher/create";
+//         $.ajax({
+//             url: url,
+//             data: {
+//                 comment: $("#comment").val(),
+//             },
+//             method: "POST"
+//         });
+//         return false;
+//     });
+//     window.Echo.channel("chat")
+//         .listen("Pusher",e =>{
+//         $("#board").append("<li>" + e.chat.comment + "</li>");
+//     });
+// });
