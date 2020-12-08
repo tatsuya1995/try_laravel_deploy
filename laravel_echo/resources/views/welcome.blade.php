6858aa503b3357bdcf42d2e649555e4665c82594
EC2 Default User <ec2-user@ip-10-0-10-10.ap-northeast-1.compute.internal>
2020-12-08 15:11:29 +0900

    pusher機能追加　テスト

--- laravel_echo/resources/views/welcome.blade.php
+++ laravel_echo/resources/views/welcome.blade.php
@@ -3,15 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{csrf_token()}}">
+
         <title>Laravel</title>
 
-        <!-- pusher用 -->
-        <script src="/js/app.js"></script>
-        <!---->
-
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
