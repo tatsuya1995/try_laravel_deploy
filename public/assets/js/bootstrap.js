0dd58d86ae5b79a67b9c0a288e6a0e40a9e699e4
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-08 23:22:09 +0900

    pusher機能追加　テスト

--- public/assets/js/bootstrap.js
+++ public/assets/js/bootstrap.js
@@ -46,7 +46,7 @@ window.Echo = new Echo({
 //                 console.log(data);
 // });
 
-$(docummnt).ready(function(){
+$(document).ready(function(){
     $.ajaxSetup({
         headers: {
             "X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")
