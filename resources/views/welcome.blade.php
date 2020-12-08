2b4893d87e69cc52e4cbae6d531c95dc8ef27074
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-08 11:00:56 +0900

    pusher機能追加　テスト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{csrf_token()}}">
         <title>Laravel</title>
+
+        <!-- pusher用 -->
+        <script src="/js/app.js"></script>
+        <!---->
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
