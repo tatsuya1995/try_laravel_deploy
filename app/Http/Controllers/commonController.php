2442553f75ef72b9d988f55e5149f6a6e947d45a
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-09 10:29:31 +0900

    pusher機能追加　テスト

--- app/Http/Controllers/commonController.php
+++ app/Http/Controllers/commonController.php
@@ -17,7 +17,7 @@ class CommonController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
