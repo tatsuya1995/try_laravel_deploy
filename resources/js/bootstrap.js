858dcb142db0eec81c120d7514abfe5aebf314ee
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-09 09:39:53 +0900

    pusher機能追加　テスト

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -29,19 +29,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
 
-window.Pusher = require('pusher-js');
-
-//window.Echo = new Echo({
-//    broadcaster: 'pusher',
-//    key: process.env.MIX_PUSHER_APP_KEY,
-//    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//    encrypted: true
-//});
-
-// window.Echo.channel('chat')
-//             .listen('Pusher',function(data){
-//                 console.log('received a message');
-//                 console.log(data);
-// });
