61f9c74e9a17a400a05ed73f7dd70f8f0fcd19e0
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-14 23:53:17 +0900

    pusher機能追加　テスト

--- resources/views/driver/talk.blade.php
+++ resources/views/driver/talk.blade.php
@@ -72,13 +72,15 @@
             let appendText;
             let login = $('input[name="login"]').val();
 
-            if(data.idDriver === login){
-                appendText = '<div class="idDriver" style="text-align:right"><p>' + data.comment + '</p></div> ';
-            }else if(data.idOwner === login){
-                appendText = '<div class="idOwner" style="text-align:left"><p>' + data.comment + '</p></div> ';
-            }else{
-                return false;
-            }
+
+            appendText = '<div class="idOwner" style="text-align:left"><p>' + data.comment + '</p></div> ';
+            // if(data.idDriver === login){
+            //     appendText = '<div class="idDriver" style="text-align:right"><p>' + data.comment + '</p></div> ';
+            // }else if(data.idOwner === login){
+            //     appendText = '<div class="idOwner" style="text-align:left"><p>' + data.comment + '</p></div> ';
+            // }else{
+            //     return false;
+            // }
 
             //メッセージを表示
             $("#room").append(appendText);
