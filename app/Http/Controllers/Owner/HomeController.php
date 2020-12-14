8416d26d561ca3f7c73a1e9700cabcc280a8f969
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-14 18:13:56 +0900

    pusher機能追加　テスト

--- app/Http/Controllers/Owner/HomeController.php
+++ app/Http/Controllers/Owner/HomeController.php
@@ -182,7 +182,7 @@ class HomeController extends Controller
         //イベント発火
         event(new Pusher($request->all()));
 
-        return view('owner/talk',compact('idDriver'));
+        return redirect('owner/talk',compact('idDriver'));
     }
 
     public function talk(Request $request, $idDriver)
