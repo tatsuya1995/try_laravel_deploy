4d4e35e3e2d7727e30c373f117d674dc92353859
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-14 15:15:51 +0900

    pusher機能追加　テスト

--- app/Http/Controllers/Owner/HomeController.php
+++ app/Http/Controllers/Owner/HomeController.php
@@ -155,7 +155,7 @@ class HomeController extends Controller
             $query->where('idDriver',$idDriver);
         });
         $posts = $query->get();
-        //dd($posts);
+        dd($driverInfo);
         return view('driver/talk',compact('ownerInfo','driverInfo','posts'));
     }
 
