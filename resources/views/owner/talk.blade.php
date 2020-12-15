48f2604608e9948f88e1a6267c3dc2feb651bba3
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-15 11:57:59 +0900

    pusher機能追加　テスト

--- resources/views/owner/talk.blade.php
+++ resources/views/owner/talk.blade.php
@@ -42,10 +42,12 @@
                         @foreach($posts as $key => $post)
                             @if($post->sort === 1)
                                 <div class="driver" style="text-align:left">
+                                <p><img src="{{$driverInfo->iconDriver}}" class="iconImg" alt="ドライバーアイコン画像">、{{$driverInfo->nameDriver}}さん　{{$post->created_at}}</p>
                                     <p>{{$post->comment}}</p>
                                 </div>
                             @elseif($post->sort === 0)
                             <div class="owner" style="text-align:right">
+                            <p><img src="{{$ownerInfo->iconOwner}}" class="iconImg" alt="オーナーアイコン画像">、{{$ownerInfo->nameOwner}}さん　{{$post->created_at}}</p>
                                 <p>{{$post->comment}}</p>
                             </div>
                             @endif
