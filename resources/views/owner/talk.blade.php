23a0c214967d6fba80469b2665dff65ea01fffbc
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-19 16:09:54 +0900

    talk画面修正　固定スクロール修正

--- resources/views/owner/talk.blade.php
+++ resources/views/owner/talk.blade.php
@@ -5,16 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">ドライバー：{{$driverInfo->nameDriver}}さんとのトークルーム</div>
+                <div class="card-header">ドライバー：<img src="{{$driverInfo->iconDriver}}" class="iconImgTalktop"  alt="アイコン画像"> {{$driverInfo->nameDriver}}さんとのトークルーム</div>
                     <div class="card-body">
                         <div class ="row">
-                            <div class="col-md-6 text-center">
-                                <ul class="marker">
-                                    <li><img src="{{$driverInfo->iconDriver}}" class="iconImgTalktop"  alt="アイコン画像">
-                                    <li>氏名:{{$driverInfo->nameDriver}}
-                                </ul>
-                            </div>
-                            <div class="col-md-6 d-flex align-items-end">
+                            <div class="col-md-12 d-flex align-items-end">
                                 <form action="{{route('owner.contract',['idDriver'=> $driverInfo->id])}}" method="get">
                                     @csrf
                                     <div>契約を結ぶ（確認画面へ移動）<input type="image" src="{{asset('assets/image/arrow2.png')}}" id="arrow2"  alt="矢印画像"></div>
@@ -29,17 +23,7 @@
         <div class="col-md-10">
             <div class="card">
                 <div class="card-body"> 
-                    <div class="row">
-                        <form>
-                        @csrf
-                            <textarea name="comment" cols="40" rows="3" placeholder="こちらにメッセージを入力"></textarea>
-                            <button type="button" id="send">送信</button>
-                        </form>
-                        <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
-                        <input type="hidden" name='idOwner' value="{{$ownerInfo->id}}">
-                        <input type="hidden" name="login" value="{{Auth::id()}}">
 
-                    </div>
                     <div id="room">
                         @foreach($posts as $key => $post)
                         
@@ -57,9 +41,18 @@
                         
                         @endforeach
                     </div>
-                <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
-                <input type="hidden" name="idOwner" value="{{$ownerInfo->id}}">
-                <input type="hidden" name="login" value="{{Auth::id()}}">
+
+                <div id="fixed" class="row">
+                    <form>
+                        @csrf
+                        <textarea name="comment" cols="70" rows="3" placeholder="メッセージを入力"></textarea>
+                        <button type="button" id="send">送信</button>
+                    </form>
+                    <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
+                    <input type="hidden" name='idOwner' value="{{$ownerInfo->id}}">
+                    <input type="hidden" name="login" value="{{Auth::id()}}">
+                </div>
+
 
     <script src="/js/app.js"></script>
     <script>
