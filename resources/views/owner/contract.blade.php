cda71093a9d30b23a7dbd8b8a5a26ecc730a5b94
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-19 18:47:28 +0900

    contract画面修正

--- resources/views/owner/contract.blade.php
+++ resources/views/owner/contract.blade.php
@@ -7,6 +7,9 @@ th {
 td {
     width:80%;
 }
+ul {
+    list-style:none;
+}
 </style>
 
 
@@ -17,11 +20,14 @@ td {
             <div class="card">
                 <div class="card-header">契約確認</div>
                     <div class="card-body">
-                    <table class="table">
-                    <form action="/owner/mailContract" method="get"> 
+                        </div>
+                        <a href="{{route('index')}}">トークルームへ戻る</a>
+                        </div>
+                        <table class="table">
+                        <form action="/owner/mailContract" method="get"> 
                         @csrf             
-                        <tr><th class="">ドライバー</th><td class=""><img src="{{$driverInfo->iconDriver}}" class="iconImgContract" alt="ドライバーアイコン画像">　{{$driverInfo->nameDriver}}　様</td></tr>
-                        <tr><th class="">使用開始時間</th><td class="">
+                        <tr><th>ドライバー</th><td><img src="{{$driverInfo->iconDriver}}" class="iconImgContract" alt="ドライバーアイコン画像">　{{$driverInfo->nameDriver}}　様</td></tr>
+                        <tr><th>使用開始時間</th><td>
                                                     <dt>
                                                         <dd>
                                                             <input type="date" name="dateDeparture">
@@ -29,7 +35,7 @@ td {
                                                         </dd>
                                                     </dt>
                                                 </td></tr>
-                        <tr><th class="">使用終了時間</th><td class="">
+                        <tr><th>使用終了時間</th><td>
                                                     <dt>
                                                         <dd>
                                                             <input type="date" name="dateRevert">
@@ -37,20 +43,20 @@ td {
                                                         </dd>
                                                     </dt>
                                                 </td></tr>
-                        <tr><th class="">車両情報</th>
-                            <td class="">
+                        <tr><th>車両情報</th>
+                            <td>
                                 <dd>車種名：{{$ownerInfo->nameCar}}（最大{{$ownerInfo->numPeople}}人乗り）
                                 <dd>ナンバープレート：<input type=text name="carNumber">（例.北九州123-あ-1234）</input>
                             </td>
                         </tr>
-                        <tr><th class="">使用料金 </th><td class="">
+                        <tr><th>使用料金 </th><td>
                             <dd class="text-primary">ドライバーと合意した金額を「小計」に入力後、「計算する」ボタンを押してください。
                             <dd>小　計：<input type="text" size="5"  name="subtotal" id="subtotal"> 円＋保険料500円　 <b id="outputSubtotal"></b>
                             <dd>手数料：<b id="fee"></b>（小計の10%）　
                             <dd><button type="button" class="btn-primary" id="calc">計算する</button>　<b>総計：<span id="outputTotal"></span></b>
                         </td></tr>
-                        <tr><th class=""></th>
-                            <td class="">
+                        <tr><th></th>
+                            <td>
                                 <ul>
                                     <li><input type="checkbox" name="confirm">上記の内容で問題なし
                                     <li>※メールにてドライバー確認後、最終決定となります。
@@ -64,10 +70,6 @@ td {
                         <input type="hidden" name="fee" value="fee">
                     </form>
                     </table>
-
-                    </div>
-                    <a href="{{route('index')}}">トークルームへ戻る</a>
-                    </div>
                 </div>
             </div>
         </div>
