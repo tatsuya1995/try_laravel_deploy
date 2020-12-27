0bfe2ee09b7577e6c25be23c1df04687de321048
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-27 22:23:23 +0900

    talk画面修正　css

--- resources/views/driver/talk.blade.php
+++ resources/views/driver/talk.blade.php
@@ -6,56 +6,60 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">オーナー：<img src="{{$ownerInfo->iconOwner}}" class="iconImgContract" alt="アイコン画像">{{$ownerInfo->nameOwner}}さんとのトークルーム</div>
-                    <div class="card-body">
-                        <div class ="row text-center  d-flex align-items-center">
-                            <div class="col-md-6">
-                                <ul class="marker">
-                                    <li><img src="{{$ownerInfo->imgCar}}" class="carImg" alt="車両画像">
-                                </ul>
-                            </div>
-                            <div class="col-md-6">
-                                <ul class="marker carDetail">
-                                    <li>車名：{{$ownerInfo->nameCar}}
-                                    <li>最大乗車可能人数：{{$ownerInfo->numPeople}}人
-                                </ul>
-                            </div>
+                <div class="card-header"　id="card-header-talk" >
+                    トーク：<img src="{{$ownerInfo->iconOwner}}" class="iconImgContract" alt="アイコン画像"> {{$ownerInfo->nameOwner}} さん
+                </div>
+                <div class="card-body">
+                    <div class ="row text-center  d-flex align-items-center">
+                        <div class="col-md-6">
+                            <ul class="marker">
+                                <li><img src="{{$ownerInfo->imgCar}}" class="carImg" alt="車両画像">
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="marker carDetail">
+                                <li>車名：{{$ownerInfo->nameCar}}
+                                <li>最大乗車可能人数：{{$ownerInfo->numPeople}}人
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
                 <div class="card-body"> 
-
-                    <div id="room">
-                        @foreach($posts as $key => $post)
-                            @if($post->sort === 1)
-                                <div class="driver" style="text-align:left">
-                                <p><img src="{{$driverInfo->iconDriver}}" class="iconImgTalk" alt="ドライバーアイコン画像">　{{$driverInfo->nameDriver}}さん　○○○○○○○○○{{$post->created_at}}</p>
-                                    <p>{{$post->comment}}</p>
-                                </div>
-                            @elseif($post->sort === 0)
-                                <div class="owner" style="text-align:right">
-                                <p><img src="{{$ownerInfo->iconOwner}}" class="iconImgTalk" alt="オーナーアイコン画像">　{{$ownerInfo->nameOwner}}さん　○○○○○○○○○{{$post->created_at}}</p>
-                                    <p>{{$post->comment}}</p>
-                                </div>
-                            @endif
-                        @endforeach
+                    @if ($posts->isEmpty())
+                    <p>※メッセージを入力してください。</br>(例：○月○日に車両をお貸しいただけないでしょうか？5時間で3000円程度でお願いしたいです。)</p>
+                    @else
+                        <div id="room">
+                            @foreach($posts as $key => $post)
+                                @if($post->sort === 1)
+                                    <div class="driver" style="text-align:left">
+                                        <p><img src="{{$driverInfo->iconDriver}}" class="iconImgTalk" alt="ドライバーアイコン画像">　{{$driverInfo->nameDriver}}さん</p>
+                                        <p>{{$post->comment}}</p>
+                                    </div>
+                                @elseif($post->sort === 0)
+                                    <div class="owner" style="text-align:right">
+                                        <p><img src="{{$ownerInfo->iconOwner}}" class="iconImgTalk" alt="オーナーアイコン画像">　{{$ownerInfo->nameOwner}}さん</p>
+                                        <p>{{$post->comment}}</p>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    @endif
+                    <div class="row" id="fixed">
+                        <form>
+                            @csrf
+                            <textarea id="textarea" name="comment"  placeholder="メッセージを入力"></textarea>     
+                            <button type="button" class="btn btn-primary" id="send">送信</button>
+                        </form>
+                            <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
+                            <input name='idOwner' type="hidden" value="{{$ownerInfo->id}}">
+                            <input type="hidden" name="login" value="{{Auth::id()}}">
                     </div>
-             <div id="fixed">
-                <form>
-                    @csrf
-                    <textarea id="textarea" name="comment" cols="70" rows="3" placeholder="メッセージを入力"></textarea>     
-                    <button type="button" class="btn btn-primary" id="send">送信</button>
-                </form>
-                    <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
-                    <input name='idOwner' type="hidden" value="{{$ownerInfo->id}}">
-                    <input type="hidden" name="login" value="{{Auth::id()}}">
-                </div>
-            </div>
                 </div>
             </div>
         </div>
