9901d36c00422d4f0c7a28ccabde7eb83255bd6a
tatsuya1995 <tatsuyawada1995@gmail.com>
2020-12-27 23:20:18 +0900

    talk画面修正

--- resources/views/owner/talk.blade.php
+++ resources/views/owner/talk.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">ドライバー：<img src="{{$driverInfo->iconDriver}}" class="iconImgContract"  alt="アイコン画像"> {{$driverInfo->nameDriver}}さんとのトークルーム</div>
+                <div class="card-header">トーク：<img src="{{$driverInfo->iconDriver}}" class="iconImgContract"  alt="アイコン画像"> {{$driverInfo->nameDriver}} さん</div>
                     <div class="card-body">
                         <div id="room">
                         @foreach($posts as $key => $post)
@@ -27,20 +27,20 @@
                         </div>
 
                     <div class="row" id="fixed">
-                    <form>
-                        @csrf
-                        <textarea id="textarea" name="comment"  placeholder="メッセージを入力"></textarea>     
-                        <button type="button" class="btn btn-primary" id="send">送信</button>
-                    </form>
                     <div class="text-right align-items-end">
                         <form action="{{route('owner.contract',['idDriver'=> $driverInfo->id])}}" method="get">
                             @csrf
                             <div>契約を結ぶ（確認画面へ移動）<input type="image" src="{{asset('assets/image/arrow2.png')}}" id="arrow2"  alt="矢印画像"></div>
                         </form>
                     </div>
-                    <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
-                    <input type="hidden" name='idOwner' value="{{$ownerInfo->id}}">
-                    <input type="hidden" name="login" value="{{Auth::id()}}">
+                    <form>
+                        @csrf
+                        <textarea id="textarea" name="comment"  placeholder="メッセージを入力"></textarea>     
+                        <button type="button" class="btn btn-primary" id="send">送信</button>
+                    </form>
+                        <input type="hidden" name="idDriver" value="{{$driverInfo->id}}">
+                        <input type="hidden" name='idOwner' value="{{$ownerInfo->id}}">
+                        <input type="hidden" name="login" value="{{Auth::id()}}">
                 </div>
 
     <script src="/js/app.js"></script>
